6d0fa13ee4c92bb2b1d55db47b0681171b28b791
carlos-alexandrebp <java-util@hotmail.cor.email>
2019-02-06 20:22:25 -0200

    Projeto Criado Aprendizagem 'Generics'

---
+++ src/program/Dados.java
@@ -0,0 +1,80 @@
+package program;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dados <T> {
+    private int quantidade;
+    private String tipoDeDados;
+    
+    List<T> dados = new ArrayList<>();
+
+    public void add(T valor)
+    {
+        dados.add(valor);
+        
+    }
+    public String print(){
+        String println ="";
+        
+        for(int a = 0 ; a < dados.size() ; a++)
+        {
+            
+            if(a == 0){
+                println = "[ "+dados.get(a).toString();
+                
+            }
+            else if(dados.size()-1 ==  a) {
+             println += " , "+dados.get(a).toString()+" ]";
+            } 
+            
+            else{
+                println += " , "+dados.get(a).toString();
+            
+            
+            }
+            
+        
+        }
+        
+        return println;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getTipoDeDados() {
+        return tipoDeDados;
+    }
+
+    public void setTipoDeDados(String tipoDeDados) {
+        this.tipoDeDados = tipoDeDados;
+    }
+    
+    
+}
---
+++ src/program/Run.java
@@ -0,0 +1,97 @@
+package program;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Run {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Scanner scs = new Scanner(System.in);
+        Dados<String> dadosSTR = new Dados<>();
+        Dados<Integer> dadosINT = new Dados<>();
+        Dados<Double> dadosDOU = new Dados<>();
+        
+        
+        System.out.print("Digite o tipo de dados a trabalhar no sistema [Str / Dou / Int ] :");
+        String opcao = sc.next();
+        opcao = opcao.toUpperCase();
+        System.out.println("");
+         int qnt = -1;
+         
+        switch(opcao){
+            case "STR" :
+                System.out.print("Digite a quantidade de String para salvar: ");
+               
+               
+                try{
+                    qnt = sc.nextInt();
+                    System.out.println("");
+                }
+                catch(Exception erro ) 
+                {
+                    System.out.println("Erro Seguir com o programa assim mesmo: [y/s]: " );
+                    
+                }
+                while(qnt != 0){
+                
+                    System.out.print(">: ");
+                    dadosSTR.add(scs.nextLine());
+                    qnt--;
+                }
+                    
+                    
+                System.out.println(dadosSTR.print()); 
+                    
+                    break;
+            case "INT":
+                System.out.print("Digite a quantidade de Inteiros para salvar: ");
+               
+                
+                try{
+                    qnt = sc.nextInt();
+                    System.out.println("");
+                }
+                catch(Exception erro ) 
+                {
+                    System.out.println("Erro Seguir com o programa assim mesmo: [y/s]: " );
+                    
+                }
+                while(qnt != 0){
+                
+                    System.out.print(">: ");
+                    dadosINT.add(scs.nextInt());
+                    qnt--;
+                }
+                    
+                    
+                System.out.println(dadosINT.print()); break;
+           
+            case "DOU" :
+                System.out.print("Digite a quantidade de Double para salvar: ");
+               
+                
+                try{
+                    qnt = sc.nextInt();
+                    System.out.println("");
+                }
+                catch(Exception erro ) 
+                {
+                    System.out.println("Erro Seguir com o programa assim mesmo: [y/s]: " );
+                    
+                }
+                while(qnt != 0){
+                
+                    System.out.print(">: ");
+                    dadosDOU.add(scs.nextDouble());
+                    qnt--;
+                }
+                    
+                    
+                System.out.println(dadosDOU.print()); break;
+        
+        
+        
+        }
+    }
+}
